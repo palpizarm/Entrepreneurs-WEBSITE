96b3e52869cbdba5265c2581595024fc0c13b709
fabianmartinez04 <54187391+fabianmartinez04@users.noreply.github.com>
2021-06-15 23:13:56 -0600

    Historial de compras y ventas

--- API-REST/routes/order.js
+++ API-REST/routes/order.js
@@ -41,6 +41,67 @@ router.post('/insertOrder', async(req,res) => {
     }
 })
 
+//Historial de compras de un usuario
+
+router.post('/getBuyHistory', async(req,res) => {
+    try {
+        let poolEB = await sql.connect(dbElbarrio);
+        
+        let products =  await poolEB.request()
+            .query(`SELECT o.id_order, o.date, o.state, ol.id_order_line, ol.id_item, ol.quantity, ol.unit_price,
+            ol.total, i.id_shop, i.name AS itemName, i.image AS itemImage, s.name AS shopName
+            FROM CUSTOMERS c inner join "ORDER" o ON c.id_customer = o.id_customer 
+            inner join "ORDER_LINE" ol ON o.id_order = ol.id_order
+            inner join ITEM i ON ol.id_item = i.id_item
+            inner join SHOP s ON i.id_shop = s.id_shop 
+            WHERE c.id_customer = ${req.body.id_customer}`);
+        
+        res.json({
+            code : 1,
+             msg : '',
+            data : products.recordsets[0]
+        });
+        
+    }
+    catch (error) {
+        res.json({
+            code : -8,
+            msg : 'Intentelo nuevamente, no se pudo mostrar el historial de compra.',
+            data : error
+        });
+    }
+})
+
+//Historial de venta
+
+router.post('/getSentHistory', async(req,res) => {
+    try {
+        let poolEB = await sql.connect(dbElbarrio);
+        
+        let products =  await poolEB.request()
+            .query(`SELECT u.id_user, u.name AS userName, u.email, u.phone, o.id_order, o.date, o.state, ol.id_order_line, ol.id_item, ol.quantity, ol.unit_price,
+            ol.total, i.id_shop, i.name AS itemName, i.image AS itemImage
+            FROM USERS u inner join CUSTOMERS c ON u.id_user = c.id_customer inner join "ORDER" o ON c.id_customer = o.id_customer 
+            inner join "ORDER_LINE" ol ON o.id_order = ol.id_order
+            inner join ITEM i ON ol.id_item = i.id_item
+            inner join SHOP s ON i.id_shop = s.id_shop 
+            WHERE i.id_shop = ${req.body.id_shop}`);
+        
+        res.json({
+            code : 1,
+             msg : '',
+            data : products.recordsets[0]
+        });
+        
+    }
+    catch (error) {
+        res.json({
+            code : -8,
+            msg : 'Intentelo nuevamente, no se pudo mostrar el historial de ventas.',
+            data : error
+        });
+    }
+})
 
 
 module.exports = router
