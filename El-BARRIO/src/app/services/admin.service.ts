312a083283e781f1d9efc7933e64df44da2be5ec
fabianmartinez04 <54187391+fabianmartinez04@users.noreply.github.com>
2021-06-16 00:39:02 -0600

    Services Admin

--- El-BARRIO/src/app/services/admin.service.ts
+++ El-BARRIO/src/app/services/admin.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,33 @@ import { Injectable } from '@angular/core';
 })
 export class AdminService {
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
+
+  registerQues(id_customer:number,
+    question:string)
+  {
+  const url = `http://localhost:3000/questionToAdmin/registerQuestion`;
+  const body = {
+    'id_customer':id_customer,
+    'question':question
+  }
+  return this.http.post(url,body);
+  }
+
+
+  getShowQuestions() {
+    const url = `http://localhost:3000/questionToAdmin/showQuestions`;
+    return this.http.post(url,{});
+  }
+
+  changeQuestionAnswer(answer:string,id_question:number) {
+    const url =  `http://localhost:3000/questionToAdmin/updateQuestion`;
+    return this.http.post(url,{'answer':answer, 'id_question':id_question});
+  }
+
+  getQuestionsConsumer(q:number) {
+    const url = `http://localhost:3000/questionToAdmin/showQuestionsConsumer`;
+    return this.http.post(url,{'id_customer':q});
+  }
+
 }
